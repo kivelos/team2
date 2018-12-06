8329a9d240c98e1300b67ec648cefcaf83f21966
ksenya96 <k.pilyak96@tut.by>
2018-12-06 09:28:33 +0200

    added spring security

---
+++ src/main/java/dev/java/controller/HelloController.java
@@ -0,0 +1,15 @@
+package dev.java.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class HelloController {
+    @GetMapping("/")
+    public String hello(HttpServletRequest request) {
+        System.out.println(request.isUserInRole("ADMIN"));
+        return "index";
+    }
+}
